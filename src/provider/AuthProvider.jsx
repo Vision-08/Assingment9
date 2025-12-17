00b97bf36f9c14208533437560754761979d9b41
Essa Zaman <essazamanamin102@gmail.com>
2025-12-18 00:06:03 +0600

    Implement AuthProvider context

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,85 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    updateProfile,
+    GoogleAuthProvider,
+    signInWithPopup,
+    sendPasswordResetEmail
+} from "firebase/auth";
+import auth from "../firebase/firebase.init";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const googleProvider = new GoogleAuthProvider();
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    };
+
+    const updateUserProfile = (updatedData) => {
+        return updateProfile(auth.currentUser, updatedData);
+    };
+
+    const passwordReset = (email) => {
+        return sendPasswordResetEmail(auth, email);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+            console.log("Current User:", currentUser);
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    const authInfo = {
+        user,
+        loading,
+        setLoading,
+        createUser,
+        signInUser,
+        signInWithGoogle,
+        logOut,
+        updateUserProfile,
+        passwordReset
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
